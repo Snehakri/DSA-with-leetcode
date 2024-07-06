8195d319de5f8dbadcc6a1268bdd484e4bec4dc2
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-06 16:22:25 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,10 +1,16 @@
 class Solution {
  public:
   int passThePillow(int n, int time) {
-    time %= (n - 1) * 2;
-    if (time < n)
-      return 1 + time;
+//     time %= (n - 1) * 2;
+//     if (time < n)
+//       return 1 + time;
       
-    return n - (time - (n - 1));
+//     return n - (time - (n - 1));
+      
+      int totalround=time/(n-1);
+      if(totalround%2!=0){
+         return n-(time%(n-1));
+      }
+      else return time%(n-1)+1;
   }
 };
