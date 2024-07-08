9ceab3f76110952f6cd30d8ea2c5d612b8d21357
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-08 19:06:02 +0530

    Time: 0 ms (100.00%), Space: 14.5 MB (87.17%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,34 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         next_permutation(nums.begin(),nums.end());
+//     }
+// };
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        int max=nums[n-1];
+        int ind=-1;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<max){
+                ind=i;
+                break;
+            }
+            max=nums[i];
+        }
+        if(ind!=-1){
+    
+        for(int i=n-1;i>=0;i--){
+            if(nums[ind]<nums[i]){
+                swap(nums[ind],nums[i]);
+                    break;
+            }
+        }
+        }
+        sort(nums.begin()+ind+1,nums.end());
+        
+        
     }
 };
