c8b271adf936b3736eb179421481dc8e9f23a02f
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-12 15:30:06 +0530

    Time: 9 ms (20.84%), Space: 8.2 MB (22.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double s=pow(x,n);
-        return s;
+        double ans=1;
+        long nn=n;
+        if(n<0)  nn=-nn;
+       
+        while(nn){
+            if(nn%2==1){
+                ans=ans*x;
+                 nn-=1;
+            }
+            else{
+                x*=x;
+                nn=nn/2;
+            }
+             cout<<nn<<" "<<ans<<endl;
+        }
+        if(n<0) ans=1/ans;
+        return ans;
+        
     }
 };
