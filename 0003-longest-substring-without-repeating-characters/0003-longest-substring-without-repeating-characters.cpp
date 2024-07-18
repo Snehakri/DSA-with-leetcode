c29d65db6b7c5b5cc7f983e59a8935bd8374782f
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-18 10:33:36 +0530

    Time: 102 ms (8.92%), Space: 12.1 MB (39.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,22 +8,25 @@ public:
         int i=0,j=0;
         while(i<=j && j<s.size()){
             if(m.find(s[j])!=m.end()){
-                //cout<<"uj";
-                while(i<j && m.find(s[j])!=m.end()){
-                    m.erase(s[i]);
-                    i++;
-                }
+                if(m[s[j]]>=i){
+                    i=m[s[j]]+1;
+                } 
+                //i=m[s[j]]+1;
+                // while(i<j && m.find(s[j])!=m.end()){
+                //     m.erase(s[i]);
+                //     i++;
+                // }
             }
-            m[s[j]]++;
+            m[s[j]]=j;
             count=j-i+1;
             // j++;
-            //cout<<i<<"=i, count in "<<j<<"is:"<<count<<endl;
+            cout<<i<<"=i, count in "<<j<<"is:"<<count<<endl;
             j++;
             maxi=max(maxi,count);
         }
-        // for(auto it:m){
-        //     cout<<it.first<<" ";
-        // }
+        for(auto it:m){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
         return maxi;
     }
 };
