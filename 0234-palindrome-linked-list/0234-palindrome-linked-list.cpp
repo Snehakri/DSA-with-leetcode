e58c3b4612a173a96e87f683a1453f7b8916bee1
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-19 20:00:49 +0530

    Time: 153 ms (84.73%), Space: 116.8 MB (67.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,37 +11,70 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *slow=head;
-        ListNode *fast=head;
-        while(fast && fast->next){
-            fast=fast->next->next;
-            slow=slow->next;
+        if(!head || !head->next) return true;
+        ListNode* s=head,*f=head;
+        while(f && f->next){
+            s=s->next;
+            f=f->next->next;
         }
-        if(fast){
-            slow=slow->next;
+    
+        ListNode *p=NULL;
+        while(s){
+            ListNode *temp=s->next;
+            s->next=p;
+            p=s;
+            s=temp;
         }
-        slow=reverse_list(slow);
-        fast=head;
-        while(slow){
-            if(fast->val!=slow->val) return false;
-            fast=fast->next;
-            slow=slow->next;
+        // while(p){
+        //     cout<<p->val;
+        //     p=p->next;
+        // }
+        // cout<<endl;
+        // while(head){
+        //     cout<<head->val<<" ";
+        //     head=head->next;
+        // }
+        // p and head compare
+        while(p && head){
+            //cout<<p->val<<" and "<<head->val<<endl;
+            if(p->val!=head->val){
+                return false; 
+            }
+            p=p->next;
+            head=head->next;
         }
         return true;
+        // ListNode *slow=head;
+        // ListNode *fast=head;
+        // while(fast && fast->next){
+        //     fast=fast->next->next;
+        //     slow=slow->next;
+        // }
+        // if(fast){
+        //     slow=slow->next;
+        // }
+        // slow=reverse_list(slow);
+        // fast=head;
+        // while(slow){
+        //     if(fast->val!=slow->val) return false;
+        //     fast=fast->next;
+        //     slow=slow->next;
+        // }
+        // return true;
     }
-    ListNode* reverse_list(ListNode* p){
+//     ListNode* reverse_list(ListNode* p){
         
-        // ListNode *p=head;
-        ListNode *q,*r;
-        r=NULL;
-        while(p){
-            q=p->next;
-            p->next=r;
-            r=p;
-            p=q;
-        }
+//         // ListNode *p=head;
+//         ListNode *q,*r;
+//         r=NULL;
+//         while(p){
+//             q=p->next;
+//             p->next=r;
+//             r=p;
+//             p=q;
+//         }
         
-        return r;
+//         return r;
         
-    }
+//     }
 };
