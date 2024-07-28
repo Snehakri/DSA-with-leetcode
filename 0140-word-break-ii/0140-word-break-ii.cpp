fa775cfd023a79000101429c3dbbe6714dca291f
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-28 13:11:44 +0530

    Time: 0 ms (100.00%), Space: 10 MB (12.23%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,33 +1,23 @@
 class Solution {
  public:
-  vector<string> wordBreak(string s, vector<string>& wordDict) {
-    unordered_set<string> wordSet{wordDict.begin(), wordDict.end()};
-    unordered_map<string, vector<string>> mem;
-    return wordBreak(s, wordSet, mem);
-  }
-
- private:
-  vector<string> wordBreak(const string& s,
-                           const unordered_set<string>& wordSet,
-                           unordered_map<string, vector<string>>& mem) {
-    if (const auto it = mem.find(s); it != mem.cend())
-      return it->second;
-
-    vector<string> ans;
-
-    // 1 <= prefix.length() < s.length()
-    for (int i = 1; i < s.length(); ++i) {
-      const string& prefix = s.substr(0, i);
-      const string& suffix = s.substr(i);
-      if (wordSet.count(prefix))
-        for (const string& word : wordBreak(suffix, wordSet, mem))
-          ans.push_back(prefix + " " + word);
-    }
-
-    // `wordSet` contains the whole string s, so don't add any space.
-    if (wordSet.count(s))
-      ans.push_back(s);
-
-    return mem[s] = ans;
+      
+    unordered_map<string,vector<string>> dp;
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> result;
+       if(dp.find(s)!=dp.end()) return dp[s];
+        for(string w:wordDict){
+            if(s.substr(0,w.size())==w){
+                if(w.size()==s.size()){
+                    result.push_back(w);
+                }
+                else{
+                    vector<string> temp=wordBreak(s.substr(w.length()),wordDict);
+                    for(string t:temp)
+                        result.push_back(w+" "+t);
+                }
+            }
+        }
+        return dp[s]=result;
+        
   }
 };
