db59a000263b798dcf15f7ac275427793742ba87
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-07-31 16:11:21 +0530

    Time: 17 ms (72.90%), Space: 25 MB (5.26%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         if(nums.size()==1) return nums[0];
+//         int low=0,high=nums.size()-1;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             if(mid==0) return nums[0];
+//             if( nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+//                 return nums[mid];
+//             }
+//             else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
+//                 low=mid+1;
+//             }
+//             else {
+//                 high=mid-1;
+//             }
+//         }
+//         cout<<low<<" and "<<high;
+//         return nums[low];
+//     }
+// };
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        int low=0,high=nums.size()-1;
+        int n=nums.size();
+        int low=0,high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
-            if(mid==0) return nums[0];
-            if( nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
+            if(nums[mid]==nums[mid^1]){
                 low=mid+1;
             }
-            else {
-                high=mid-1;
-            }
+            else high=mid-1;
         }
-        cout<<low<<" and "<<high;
         return nums[low];
     }
 };
