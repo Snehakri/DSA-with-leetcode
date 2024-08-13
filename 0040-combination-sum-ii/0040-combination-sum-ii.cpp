0839ac7dcd5c2a57a613615993d87ec555f16a26
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2024-08-13 14:48:23 +0530

    Time: 28 ms (8.62%), Space: 19.1 MB (9.43%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,25 +1,70 @@
+// class Solution {
+// public:
+//     void solve(int it,int target,vector<int>& candidates,int n,vector<int> temp,vector<vector<int>>&ans){
+//         if(target==0){
+//             ans.push_back(temp);
+//             return;
+//         }
+//         for(int i=it;i<n;i++){
+//             if(it!=i && candidates[i]==candidates[i-1]) continue;
+//             if(target<0) break;
+//             temp.push_back(candidates[i]);
+//             solve(i+1,target-candidates[i],candidates,n,temp,ans);
+//             temp.pop_back();
+//         }
+//         return;
+//     }
+//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//         sort(candidates.begin(),candidates.end());
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         solve(0,target,candidates,candidates.size(),temp,ans);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    void solve(int it,int target,vector<int>& candidates,int n,vector<int> temp,vector<vector<int>>&ans){
-        if(target==0){
+    void solve(int it,int sum,vector<int> temp,vector<int>& candidates, int target,vector<vector<int>> & ans){
+        if(sum==target){
             ans.push_back(temp);
             return;
         }
-        
-        for(int i=it;i<n;i++){
+        if(sum>target) return ;
+        for(int i=it;i<candidates.size();i++){
             if(it!=i && candidates[i]==candidates[i-1]) continue;
-            if(target<0) break;
             temp.push_back(candidates[i]);
-            solve(i+1,target-candidates[i],candidates,n,temp,ans);
+            sum+=candidates[i];
+            solve(i+1,sum,temp,candidates,target,ans);
+            sum-=candidates[i];
             temp.pop_back();
         }
-        return;
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(0,target,candidates,candidates.size(),temp,ans);
+        solve(0,0,temp,candidates,target,ans);
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
