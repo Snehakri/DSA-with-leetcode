10a69485e9c08789866b92b68c361ef6e9fa360e
Sneha kumari <95474769+Snehakri@users.noreply.github.com>
2026-03-04 00:12:38 +0530

    Time: 11 ms (12.77%), Space: 23.8 MB (59.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,58 @@
+// class Solution {
+// public:
+//     // static bool cmp(const vector<vector<int>> &a,const vector<vector<int>> &b){
+//     //    
+//     // }
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end());
+//         vector<vector<int>> ans;
+//         int start=intervals[0][0];
+//         int end=intervals[0][1];
+//         int n=intervals.size();
+        
+        
+//         for(int i=1;i<n;i++){
+//             if(intervals[i][0]>end){
+//                 ans.push_back({start,end});
+//                 start=intervals[i][0];
+//                 end=intervals[i][1];
+//             }
+//             else end=max(intervals[i][1],end);
+//         }
+        
+        
+//         ans.push_back({start,end});
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
-    // static bool cmp(const vector<vector<int>> &a,const vector<vector<int>> &b){
-    //     return a.second;
-    // }
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
         int start=intervals[0][0];
         int end=intervals[0][1];
-        int n=intervals.size();
-        for(int i=1;i<n;i++){
+
+        for(int i=1;i<intervals.size();i++){
             if(intervals[i][0]>end){
                 ans.push_back({start,end});
                 start=intervals[i][0];
                 end=intervals[i][1];
             }
-            else end=max(intervals[i][1],end);
+            else{
+                end=max(end,intervals[i][1]);
+            }
+            
         }
         ans.push_back({start,end});
         return ans;
+
     }
 };
